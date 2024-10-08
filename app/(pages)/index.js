9366ef72b5721e68b7f4ca387d819822189918fe
hypackel <aarav@hypackel.com>
2024-10-07 23:09:39 -0400

    @CodingKitten-YT Please edit the readme

--- app/(pages)/index.js
+++ app/(pages)/index.js
@@ -1,166 +1,198 @@
-import React, { useState, useEffect } from 'react';
-import { FlatList, Text, View, ActivityIndicator, Image, Pressable, TextInput } from 'react-native';
-import { StatusBar } from 'expo-status-bar';
-import { router } from 'expo-router';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import React, { useState, useEffect } from "react";
+import {
+	FlatList,
+	Text,
+	View,
+	ActivityIndicator,
+	Image,
+	Pressable,
+	TextInput,
+} from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { router } from "expo-router";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const defaultUrls = [
-  'https://quarksources.github.io/dist/quantumsource.min.json',
-  'https://corsproxy.io/?https%3A%2F%2Fipa.cypwn.xyz%2Fcypwn.json'
+  'https://community-apps.sidestore.io/sidecommunity.json',
+  'https://corsproxy.io/?https%3A%2F%2Fraw.githubusercontent.com%2FBalackburn%2FYTLitePlusAltstore%2Fmain%2Fapps.json',
+  'https://tiny.one/SpotC',
+  'https://repo.apptesters.org',
+  'https://randomblock1.com/altstore/apps.json',
+  'https://qnblackcat.github.io/AltStore/apps.json',
+  'https://esign.yyyue.xyz/app.json',
+  'https://bit.ly/wuxuslibraryplus',
+  'https://bit.ly/wuxuslibrary',
+	"https://raw.githubusercontent.com/vizunchik/AltStoreRus/master/apps.json",
+	"https://quarksources.github.io/dist/quantumsource.min.json",
+	"https://corsproxy.io/?https%3A%2F%2Fipa.cypwn.xyz%2Fcypwn.json",
 ];
 
 export default function App() {
-  const [appsData, setAppsData] = useState([]);
-  const [filteredData, setFilteredData] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [refreshing, setRefreshing] = useState(false); 
-  const [searchQuery, setSearchQuery] = useState('');
+	const [appsData, setAppsData] = useState([]);
+	const [filteredData, setFilteredData] = useState([]);
+	const [loading, setLoading] = useState(true);
+	const [refreshing, setRefreshing] = useState(false);
+	const [searchQuery, setSearchQuery] = useState("");
 
+	// Track pressed item state
+	const [pressedItem, setPressedItem] = useState(null);
 
-  // Track pressed item state
-  const [pressedItem, setPressedItem] = useState(null);
+	const fetchData = async (repos) => {
+		setLoading(true);
+		try {
+			const urls = [...repos, ...defaultUrls];
+			const fetchedData = await Promise.all(
+				urls.map(async (url) => {
+					const response = await fetch(url);
+					return await response.json();
+				}),
+			);
 
-  const fetchData = async (repos) => {
-    setLoading(true);
-    try {
-      const urls = [...repos, ...defaultUrls];
-      const fetchedData = await Promise.all(
-        urls.map(async (url) => {
-          const response = await fetch(url);
-          return await response.json();
-        })
-      );
+			const combinedApps = fetchedData.flatMap((source, index) => {
+				return source.apps.map((app) => ({
+					...app,
+					sourceName: source.name,
+					sourceUrl: urls[index],
+				}));
+			});
 
-      const combinedApps = fetchedData.flatMap((source, index) => {
-        return source.apps.map(app => ({
-          ...app,
-          sourceName: source.name,
-          sourceUrl: urls[index],
-        }));
-      });
+			setAppsData(combinedApps);
+			setFilteredData(combinedApps);
+		} catch (error) {
+			console.error("Error fetching data:", error);
+		} finally {
+			setLoading(false);
+			setRefreshing(false);
+		}
+	};
 
-      setAppsData(combinedApps);
-      setFilteredData(combinedApps);
-    } catch (error) {
-      console.error('Error fetching data:', error);
-    } finally {
-      setLoading(false);
-      setRefreshing(false); 
-    }
-  };
+	const loadCustomRepos = async () => {
+		const customReposJson = await AsyncStorage.getItem("customRepos");
+		return JSON.parse(customReposJson) || [];
+	};
 
-  const loadCustomRepos = async () => {
-    const customReposJson = await AsyncStorage.getItem('customRepos');
-    return JSON.parse(customReposJson) || [];
-  };
+	useEffect(() => {
+		const loadData = async () => {
+			const repos = await loadCustomRepos();
+			fetchData(repos);
+		};
+		loadData();
+	}, []);
 
-  useEffect(() => {
-    const loadData = async () => {
-      const repos = await loadCustomRepos();
-      fetchData(repos);
-    };
-    loadData();
-  }, []);
+	const onRefresh = async () => {
+		setRefreshing(true);
+		const repos = await loadCustomRepos();
+		fetchData(repos);
+	};
 
-  const onRefresh = async () => {
-    setRefreshing(true);
-    const repos = await loadCustomRepos();
-    fetchData(repos); 
-  };
+	const handleSearch = (query) => {
+		setSearchQuery(query);
+		if (query) {
+			const filtered = appsData.filter((app) =>
+				app.name.toLowerCase().includes(query.toLowerCase()),
+			);
+			setFilteredData(filtered);
+		} else {
+			setFilteredData(appsData);
+		}
+	};
 
-  const handleSearch = (query) => {
-    setSearchQuery(query);
-    if (query) {
-      const filtered = appsData.filter(app => 
-        app.name.toLowerCase().includes(query.toLowerCase())
-      );
-      setFilteredData(filtered);
-    } else {
-      setFilteredData(appsData); 
-    }
-  };
+	const renderAppItem = ({ item }) => {
+		const isPressed = pressedItem === item.bundleIdentifier;
 
- const renderAppItem = ({ item }) => {
-    const isPressed = pressedItem === item.bundleIdentifier;
+		return (
+			<Pressable
+				style={{
+					flex: 1,
+					marginHorizontal: 8,
+					opacity: isPressed ? 0.5 : 1,
+					marginBottom: 16,
+					backgroundColor: "#242424", // Change background on press
+					borderRadius: 12,
+					overflow: "hidden",
+					shadowColor: "#000",
+					shadowOffset: { width: 0, height: 2 },
+					shadowOpacity: 0.8,
+					shadowRadius: 5,
+					elevation: 4, // Card shadow for depth
+				}}
+				onPressIn={() => setPressedItem(item.bundleIdentifier)} // Set pressed item
+				onPressOut={() => setPressedItem(null)} // Reset pressed item
+				onPress={() =>
+					router.push(
+						`/detail/${item.bundleIdentifier}?source=${encodeURIComponent(item.sourceUrl)}`,
+					)
+				}
+			>
+				<View
+					style={{ flexDirection: "row", padding: 16, alignItems: "center" }}
+				>
+					<Image
+						source={{ uri: item.iconURL }}
+						style={{ borderRadius: 8, width: 64, height: 64, marginRight: 16 }}
+					/>
+					<View style={{ flex: 1 }}>
+						<Text style={{ color: "#FFF", fontWeight: "bold", fontSize: 18 }}>
+							{item.name}
+						</Text>
+						<Text style={{ color: "#CCC", marginTop: 4 }}>
+							Source: {item.sourceName}
+						</Text>
+						<Text style={{ color: "#888", marginTop: 4 }} numberOfLines={2}>
+							{item.localizedDescription}
+						</Text>
+					</View>
+				</View>
+			</Pressable>
+		);
+	};
 
-    return (
-      <Pressable
-        style={{
-          flex: 1,
-          marginHorizontal: 8,
-          opacity: isPressed ? 0.5 : 1,
-          marginBottom: 16,
-          backgroundColor: '#242424', // Change background on press
-          borderRadius: 12,
-          overflow: 'hidden',
-          shadowColor: '#000',
-          shadowOffset: { width: 0, height: 2 },
-          shadowOpacity: 0.8,
-          shadowRadius: 5,
-          elevation: 4, // Card shadow for depth
-        }}
-        onPressIn={() => setPressedItem(item.bundleIdentifier)} // Set pressed item
-        onPressOut={() => setPressedItem(null)} // Reset pressed item
-        onPress={() =>
-          router.push(`/detail/${item.bundleIdentifier}?source=${encodeURIComponent(item.sourceUrl)}`)
-        }
-      >
-        <View style={{ flexDirection: 'row', padding: 16, alignItems: 'center' }}>
-          <Image
-            source={{ uri: item.iconURL }}
-            style={{ borderRadius: 8, width: 64, height: 64, marginRight: 16 }}
-          />
-          <View style={{ flex: 1 }}>
-            <Text style={{ color: '#FFF', fontWeight: 'bold', fontSize: 18 }}>
-              {item.name}
-            </Text>
-            <Text style={{ color: '#CCC', marginTop: 4 }}>Source: {item.sourceName}</Text>
-            <Text style={{ color: '#888', marginTop: 4 }} numberOfLines={2}>
-              {item.localizedDescription}
-            </Text>
-          </View>
-        </View>
-      </Pressable>
-    );
-  };
-  
-  return (
-    <View style={{ flex: 1, backgroundColor: '#121212' }}> 
-      <View style={{ padding: 16, backgroundColor: '#1F1F1F' }}> 
-        <Text style={{ color: '#FFF', fontWeight: 'bold', fontSize: 24, marginBottom: 16 }}>Apps</Text>
-        <TextInput
-          placeholder="Search apps..."
-          placeholderTextColor="#888" 
-          value={searchQuery}
-          onChangeText={handleSearch}
-          style={{
-            height: 40,
-            borderColor: '#444',
-            borderWidth: 1,
-            borderRadius: 8,
-            paddingHorizontal: 10,
-            color: '#FFF',
-            backgroundColor: '#2C2C2C',
-            marginBottom: 16
-          }}
-        />
-      </View>
-      <Text>{"\n"}</Text>
-      {loading ? (
-        <ActivityIndicator size="large" color="#ffffff" />
-      ) : (
-        <FlatList
-          data={filteredData}
-          renderItem={renderAppItem}
-          keyExtractor={(item, index) => `${item.bundleIdentifier}-${index}`}
-          contentContainerStyle={{ paddingHorizontal: 8 }}
-          numColumns={1}
-          refreshing={refreshing} 
-          onRefresh={onRefresh} 
-        />
-      )}
+	return (
+		<View style={{ flex: 1, backgroundColor: "#121212" }}>
+			<View style={{ padding: 16, backgroundColor: "#1F1F1F" }}>
+				<Text
+					style={{
+						color: "#FFF",
+						fontWeight: "bold",
+						fontSize: 24,
+						marginBottom: 16,
+					}}
+				>
+					Apps
+				</Text>
+				<TextInput
+					placeholder="Search apps..."
+					placeholderTextColor="#888"
+					value={searchQuery}
+					onChangeText={handleSearch}
+					style={{
+						height: 40,
+						borderColor: "#444",
+						borderWidth: 1,
+						borderRadius: 8,
+						paddingHorizontal: 10,
+						color: "#FFF",
+						backgroundColor: "#2C2C2C",
+						marginBottom: 16,
+					}}
+				/>
+			</View>
+			<Text>{"\n"}</Text>
+			{loading ? (
+				<ActivityIndicator size="large" color="#ffffff" />
+			) : (
+				<FlatList
+					data={filteredData}
+					renderItem={renderAppItem}
+					keyExtractor={(item, index) => `${item.bundleIdentifier}-${index}`}
+					contentContainerStyle={{ paddingHorizontal: 8 }}
+					numColumns={1}
+					refreshing={refreshing}
+					onRefresh={onRefresh}
+				/>
+			)}
 
-      <StatusBar style="light" />
-    </View>
-  );
+			<StatusBar style="light" />
+		</View>
+	);
 }
---
+++ readme.md
@@ -0,0 +1,138 @@
+# HyStore
+
+Welcome to HyStore. Ill rite this later or @CodingKitten-YT will?
+
+## How to Build an .ipa
+
+Every few weeks, I will build an ipa file so that you do not have to go through these steps but if you want the latest build you can follow these steps
+
+<details>
+<summary>Steps</summary>
+
+Prerequisites:
+
+- A mac that can run xcode or a VM that runs OS X
+- Node.js 21
+
+Steps:
+
+1. run `npm i` in the project
+2. run `npx expo prebuild --platform ios` to create an ios build
+3. wait a little bit for it to finish
+
+### If you have a Paid Apple Developer Account ($99/year)
+
+> [!IMPORTANT]
+> I have not tested this as I do not have a paid Apple Developer Account so you may need to edit some code.
+
+1. just use Expo Application Services (EAS) to create up to 15 ios builds for free.
+
+### If you Do not have an Apple Developer Account
+
+> This does take a little longer and may run into more errors
+
+<hr>
+
+- Download Xcode from the App Store on your Mac.
+
+> If you are on MacOS beta, then you need to download Xcode-beta from Apple's website
+
+<hr>
+
+- Open the Xcode Project by clicking `Open Existing Project`
+
+<hr>
+
+![Click File Icon in XCODE](./public/1.png)
+
+- if the file icon is not already selected, then do so
+
+<hr>
+
+- Select HyStore in the file tab
+
+<hr>
+
+
+- Choose Build Phases in the tabs
+
+<hr>
+
+
+- Click `+` on the panel and select `New Run Script Phase`
+
+<hr>
+
+- Go back to your terminal and run
+
+```bash
+sudo ln -s $(which node) /usr/local/bin/node
+```
+
+This creates a symlink to the default node location that xcode is looking for to your actual node location
+
+<hr>
+
+- Then in the script, paste the following:
+
+```sh
+# Load nvm if it's not loaded
+export NVM_DIR="$HOME/.nvm"
+if [ -s "$NVM_DIR/nvm.sh" ]; then
+  source "$NVM_DIR/nvm.sh"
+fi
+
+# Ensure Node is available
+export NODE_BINARY=/usr/local/bin/node
+echo "Node found at: $NODE_BINARY"
+
+"$NODE_BINARY" -v
+
+# Run NativeWind/Tailwind CSS build command
+"$NODE_BINARY" "$PROJECT_DIR/../node_modules/tailwindcss/lib/cli.js" -i "$PROJECT_DIR/../global.css" -o "$PROJECT_DIR/../node_modules/.cache/nativewind/global.css"
+
+# Continue with the React Native build script
+"$NODE_BINARY" --print "require('path').dirname(require.resolve('react-native/package.json')) + '/scripts/react-native-xcode.sh'"
+```
+
+<hr>
+
+- Then At the top bar select product>archive and wait for the build to complete
+
+<hr>
+
+- then it should popup with a menu if the build is successful and right click the build and click show in finder
+
+<hr>
+
+- it should be a .xcarchive so right click and select show package contents and then select products>Applications
+
+<hr>
+
+- Then right click and select `New Folder` and type `Payload` (caps senstitive) and press enter
+
+<hr>
+
+- then drag HyStore into the Payload folder
+
+<hr>
+
+- then right click and select `Compress 'Payload'` and wait
+
+<hr>
+
+- then it should pop out a .zip. so please rename the package to a .ipa so it should be `Payload.ipa` from `Payload.zip`
+
+<hr>
+
+- Enjoy!
+
+</details>
+
+## Contribututing
+
+idfk just trust
+
+## License
+
+I also dfk so just figure it out
